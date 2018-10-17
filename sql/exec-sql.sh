1abf8a3fb2d9474dd7e93740aa19308c8fb7baff
dolphinxwc <1583788680@qq.com>
2018-10-17 11:32:45 +0800

    Update exec-sql.sh
    Execute db_tars_web.sql when create the databases, which will create tables for db_tars_web database.

--- sql/exec-sql.sh
+++ sql/exec-sql.sh
@@ -3,4 +3,4 @@ mysql -uroot -proot@appinside -e "create database tars_stat"
 mysql -uroot -proot@appinside -e "create database tars_property"
 mysql -uroot -proot@appinside -e "create database db_tars_web"
 mysql -uroot -proot@appinside db_tars < db_tars.sql
-
+mysql -uroot -proot@appinside db_tas_web < db_tars_web.sql
