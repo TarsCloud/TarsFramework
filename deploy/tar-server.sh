88b806a03d213947664f9a8ccbe2801bad476d0b
ruanshudong <ruanshudong@qq.com>
2019-12-19 20:48:02 +0800

    reduce docker size

--- deploy/tar-server.sh
+++ deploy/tar-server.sh
@@ -7,3 +7,7 @@ cd ${TARS_INSTALL}/framework/servers;
 for var in ${TARS[@]} 
   do tar czf ${var}.tgz ${var} 
 done
+
+mkdir -p ${TARS_INSTALL}/web/files
+cp -rf ${TARS_INSTALL}/framework/servers/*.tgz ${TARS_INSTALL}/web/files/
+rm -rf ${TARS_INSTALL}/framework/servers/*.tgz
