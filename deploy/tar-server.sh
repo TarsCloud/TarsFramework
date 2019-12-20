c856417d5cfe4000bf5a6eef769ec329e13de057
ruanshudong <ruanshudong@qq.com>
2019-12-20 11:08:51 +0800

    fix sql lost bug

--- deploy/tar-server.sh
+++ deploy/tar-server.sh
@@ -8,6 +8,9 @@ for var in ${TARS[@]}
   do tar czf ${var}.tgz ${var} 
 done
 
+cp -rf ${TARS_INSTALL}/web/sql/*.sql ${TARS_INSTALL}/framework/sql/
+cp -rf ${TARS_INSTALL}/web/demo/sql/*.sql ${TARS_INSTALL}/framework/sql/
+ 
 mkdir -p ${TARS_INSTALL}/web/files
 cp -rf ${TARS_INSTALL}/framework/servers/*.tgz ${TARS_INSTALL}/web/files/
 rm -rf ${TARS_INSTALL}/framework/servers/*.tgz
