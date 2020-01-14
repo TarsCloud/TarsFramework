8c966b3bf16bd05523368f77c343c081fefb0693
ruanshudong <ruanshudong@qq.com>
2020-01-14 15:45:02 +0800

    fix mysql grant bug

--- deploy/tars-install.sh
+++ deploy/tars-install.sh
@@ -195,16 +195,17 @@ cd ${WORKDIR}/sql.tmp
 
 MYSQL_VER=`mysql -h${MYSQLIP} -u${USER} -p${PASS} -P${PORT} -e "SELECT VERSION();"`
 MYSQL_VER=`echo $MYSQL_VER | cut -d' ' -f2`
+MYSQL_GRANT="SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, PROCESS, REFERENCES, INDEX, ALTER, SHOW DATABASES, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER, CREATE TABLESPACE"
 
 echo "mysql version is: $MYSQL_VER"
 
 if [ `echo $MYSQL_VER|grep ^8.` ]; then
     exec_mysql_script "CREATE USER 'tars'@'%' IDENTIFIED WITH mysql_native_password BY 'tars2015';"
-    exec_mysql_script "GRANT all ON \`*\`.* TO 'tars'@'%' WITH GRANT OPTION;"
+    exec_mysql_script "GRANT ${MYSQL_GRANT} ON *.* TO 'tars'@'%' WITH GRANT OPTION;"
     exec_mysql_script "CREATE USER 'tars'@'localhost' IDENTIFIED WITH mysql_native_password BY 'tars2015';"
-    exec_mysql_script "GRANT all ON \`*\`.* TO 'tars'@'localhost' WITH GRANT OPTION;"
+    exec_mysql_script "GRANT ${MYSQL_GRANT} ON *.* TO 'tars'@'localhost' WITH GRANT OPTION;"
     exec_mysql_script "CREATE USER 'tars'@'${HOSTIP}' IDENTIFIED WITH mysql_native_password BY 'tars2015';"
-    exec_mysql_script "GRANT all ON \`*\`.* TO 'tars'@'${HOSTIP}' WITH GRANT OPTION;"
+    exec_mysql_script "GRANT ${MYSQL_GRANT} ON *.* TO 'tars'@'${HOSTIP}' WITH GRANT OPTION;"
 fi
 
 if [ `echo $MYSQL_VER|grep ^5.7` ]; then
@@ -212,14 +213,14 @@ if [ `echo $MYSQL_VER|grep ^5.7` ]; then
 fi
 
 if [ `echo $MYSQL_VER|grep ^5.` ]; then
-    exec_mysql_script "grant all on \`*\`.* to 'tars'@'%' identified by 'tars2015' with grant option;"
+    exec_mysql_script "grant ${MYSQL_GRANT} on *.* to 'tars'@'%' identified by 'tars2015' with grant option;"
     if [ $? != 0 ]; then
         LOG_DEBUG "grant error, exit." 
         exit 1
     fi
 
-    exec_mysql_script "grant all on \`*\`.* to 'tars'@'localhost' identified by 'tars2015' with grant option;"
-    exec_mysql_script "grant all on \`*\`.* to 'tars'@'$HOSTIP' identified by 'tars2015' with grant option;"
+    exec_mysql_script "grant ${MYSQL_GRANT} on *.* to 'tars'@'localhost' identified by 'tars2015' with grant option;"
+    exec_mysql_script "grant ${MYSQL_GRANT} on *.* to 'tars'@'$HOSTIP' identified by 'tars2015' with grant option;"
     exec_mysql_script "flush privileges;"
 fi
 
