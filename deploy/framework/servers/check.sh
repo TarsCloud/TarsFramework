b00449cbe9419ce2dda3903769c595d7ac7827ff
ruanshudong <ruanshudong@qq.com>
2020-03-22 20:15:22 +0800

    fix merge bug

--- deploy/framework/servers/check.sh
+++ deploy/framework/servers/check.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-echo "begin check server..."
-
-TARS_PATH=/usr/local/app/tars
-
-sh ${TARS_PATH}/tarsnode/util/check.sh
-
-#if [ "$1" == "master" ]; then
-#  TARS=(tarsAdminRegistry  tarsnode  tarsregistry)
-#else
-#  TARS=(tarsnode tarsregistry)
-#fi
-#
-#for var in ${TARS[@]};
-#do
-#  sh ${TARS_PATH}/${var}/util/check.sh
-#done
-#
-#if [ "$1" == "master" ]; then
-#  pm2 ping tars-node-web; pm2 ping tars-user-system
-#fi
+TARS_PATH/tarsnode/util/check.sh
 
 
