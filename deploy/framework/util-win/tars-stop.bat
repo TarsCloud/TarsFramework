715a3334902d3421d7c8160bc2d94163077630e4
ruanshudong <ruanshudong@qq.com>
2020-04-03 14:30:03 +0800

    fix windows tars-start.bat & tars-stop.bat, web not exists not start web

--- deploy/framework/util-win/tars-stop.bat
+++ deploy/framework/util-win/tars-stop.bat
@@ -14,5 +14,7 @@ for /f "Tokens=1,* Delims=," %%a in (%TARS%) do (
     goto :loop
 )
 
-call pm2 stop -s tars-node-web
-call pm2 stop tars-user-system
+if exist TARS_PATH\web (
+    call pm2 stop -s tars-node-web
+    call pm2 stop tars-user-system
+)
