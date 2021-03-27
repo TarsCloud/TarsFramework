8107edfb267e54d7a3de91ead012e4f6c364ecab
ruanshudong <ruanshudong@qq.com>
2021-03-27 12:32:28 +0800

    add build.Docker

--- deploy/buildInDocker.sh
+++ deploy/buildInDocker.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-if [ $# -lt 2 ]; then
-    echo $0 docker version
-    exit
-fi
+if [ $# -lt 2  ]; then
+    echo $0 version dockerfile
+    exit        
+fi              
 
-INDOCKER=$1
 SRC=`pwd`
 
-docker run -it -v $SRC:/data $INDOCKER
-docker exec $DOCKER bash -c "cd /data; mkdir -p build && cd build; "
+# cd deploy
+
+docker build . -t tars.build -f $2
+
+# docker run -v $SRC:/data tars.build bash -c "cd /data && rm -rf build-tmp; && cd bulid-tmp && cmake .. && make -j4; && make install"
+# docker run tars.build bash -c "cd /usr/local/tars/cpp/deploy && sh docker.sh $1"
+
 
