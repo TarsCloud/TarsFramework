92b6f9b10e163d8ae2bc326f7ebf8f365edc70c1
ruanshudong <ruanshudong@qq.com>
2022-01-28 21:43:05 +0800

    tarsnode add runc support

--- NodeServer/CommandStart.cpp
+++ NodeServer/CommandStart.cpp
@@ -485,7 +485,10 @@ bool CommandStart::startContainer(const ServerObjectPtr &serverObjectPtr, string
 //	return (serverObjectPtr->checkPid() == 0) ? true : false;
 }
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> tarsnode add runc support
 //////////////////////////////////////////////////////////////
 //
 int CommandStart::execute(string& sResult)
