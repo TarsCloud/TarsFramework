164ddfe6189760f7ed610586fe08293f66feb76a
ruanshudong <ruanshudong@qq.com>
2022-09-06 10:12:09 +0800

    fix deploy/docker.sh use tonistiigi/binfmt:latest

--- deploy/docker.sh
+++ deploy/docker.sh
@@ -10,7 +10,7 @@ fi
 SRC=`pwd`
 
 if [ ! -d "deploy" ]; then
-    echo "you must execute $0 in framework directory."
+    echo "you must execute $0 in framework source directory, for example: deploy/docker.sh test amd64"
     exit 1
 fi
 
@@ -20,12 +20,17 @@ if [ ! -d "web" ]; then
 fi
 
 
-export DOCKER_CLI_EXPERIMENTAL=enabled 
-docker buildx create --use --name tars-builder 
-docker buildx inspect tars-builder --bootstrap
+export DOCKER_CLI_EXPERIMENTAL=enabled
+docker run --rm --privileged tonistiigi/binfmt:latest --install all
+docker buildx create --name tars-framework-builder --use
+docker buildx inspect --bootstrap --builder tars-framework-builder
+
+#export DOCKER_CLI_EXPERIMENTAL=enabled
+#docker buildx create --use --name tars-builder
+#docker buildx inspect tars-builder --bootstrap
 #docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
 
-docker run --rm --privileged tonistiigi/binfmt:qemu-v5.2.0
+#docker run --rm --privileged tonistiigi/binfmt:qemu-v5.2.0
 
 if [ "$2" == "amd64" ]; then
     docker buildx build . --file "deploy/Dockerfile" --tag tarscloud/framework:$1 --platform=linux/amd64 -o type=docker
